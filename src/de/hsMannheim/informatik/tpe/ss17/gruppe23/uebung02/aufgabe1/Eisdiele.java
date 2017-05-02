aeed6b9d46b8189dba5a065ac4902dffb7a8564b
JoshuaJoost <joost.joshua@gmx.de>
2017-05-02 22:25:43 +0200

    Update Eisdiele.java
    Wenn ein Eis bestellt wird das nicht existiert, werden die verfügbaren Eissorten ausgegeben, danach bestellt man erneut.

--- src/de/hsMannheim/informatik/tpe/ss17/gruppe23/uebung02/aufgabe1/Eisdiele.java
+++ src/de/hsMannheim/informatik/tpe/ss17/gruppe23/uebung02/aufgabe1/Eisdiele.java
@@ -6,7 +6,7 @@ import static gdi.MakeItSimple.*;
  * This class contains an implementation of a large ice cream shop only selling ice cream cups.
  */
 public class Eisdiele {
-	
+
 	private String[] eissorten = new String[0];
 	private double[] preise = new double[0];
 	private String[] behaeltnis = new String[0];
@@ -14,13 +14,13 @@ public class Eisdiele {
 	private String[][] sorten = new String[0][];
 	private String[][] extras = new String[0][];	
 
-	
+
 	public Eisdiele() {
 		addIceCreamToMenu("Spaghettieis", 5.00, "Becher", "Spaghettis", new String[]{"Vanille"}, new String[]{"Sahne", "Erdbeersoße", "Kokosraspeln"});
 		addIceCreamToMenu("Schokoteller", 4.00, "Teller", "Bällchen", new String[]{"Schokolade", "Weisse Schokolade"}, new String[]{"Schokoladensoße", "Schokostreusel"});
 		addIceCreamToMenu("Bananasplit", 3.00, "Becher", "Kleckse", new String[]{"Banane", "Vanille"}, new String[]{"Banane", "Keks"});
 	}
-	
+
 	/**
 	 * Adds a specific ice cream to the menu of the ice cream shop.
 	 * @param name Name of the new ice cream.
@@ -37,51 +37,54 @@ public class Eisdiele {
 		}
 		newEisSorten[newEisSorten.length - 1] = name;
 		this.eissorten = newEisSorten;
-		
+
 		double[] newPreise = new double[this.preise.length + 1];
 		for(int i = 0; i < this.preise.length; i++) {
 			newPreise[i] = this.preise[i];
 		}
 		newPreise[newPreise.length - 1] = preis;
 		this.preise = newPreise;
-		
+
 		String[] newBehaeltnis = new String[this.behaeltnis.length + 1];
 		for(int i = 0; i < this.behaeltnis.length; i++) {
 			newBehaeltnis[i] = this.behaeltnis[i];
 		}
 		newBehaeltnis[newBehaeltnis.length - 1] = behaeltnis;
 		this.behaeltnis = newBehaeltnis;
-		
+
 		String[] newArt = new String[this.art.length + 1];
 		for(int i = 0; i < this.art.length; i++) {
 			newArt[i] = this.art[i];
 		}
 		newArt[newArt.length - 1] = art;
 		this.art = newArt;
-		
+
 		String[][] newSorten = new String[this.sorten.length + 1][];
 		for(int i = 0; i < this.sorten.length; i++) {
 			newSorten[i] = this.sorten[i];
 		}
 		newSorten[newSorten.length - 1] = sorten;
 		this.sorten = newSorten;
-		
+
 		String[][] newExtras = new String[this.extras.length + 1][];
 		for(int i = 0; i < this.extras.length; i++) {
 			newExtras[i] = this.extras[i];
 		}
 		newExtras[newExtras.length - 1] = extras;
 		this.extras = newExtras;
-		
+
 	}
-	
+
 	/**
 	 * Executes the process of buying a cup of ice cream.
 	 * @param typ Sort of the ordered ice cream.
 	 */
-	public void bestellen(String typ) {
-		begruessen(typ);
-		
+	public void bestellen(String typ, int orders) {
+		if(orders == 1){
+			begruessen(typ);
+		}
+		boolean exit = true;
+
 		boolean eisVerfuegbar = false;
 		int eisIndex = 0;
 		for(int i = 0; i < eissorten.length; i++) {
@@ -90,24 +93,32 @@ public class Eisdiele {
 				eisIndex = i;
 			}
 		}
-		
+
 		if(!eisVerfuegbar) {
 			entschuldigen();
+			exit = false;
 		}
 		else {
 			// Eis erstellen
 			Eis eis = new Eis(eissorten[eisIndex], preise[eisIndex], behaeltnis[eisIndex], art[eisIndex], sorten[eisIndex], extras[eisIndex]);
-			
+
 			eis.vorbereiten();
 			eis.fuellen();
 			eis.dekorieren();
-			
+
 			kassieren(eis.getPreis());
 		}
-		
-		verbschieden(eisVerfuegbar);
+
+		if(!exit){
+			println("Welches Eis möchten Sie bestellen?");
+			String orderIce = readLine();
+			bestellen(orderIce, (orders + 1));
+		}
+		else{
+			verbschieden(eisVerfuegbar);
+		}
 	}
-	
+
 	/**
 	 * Welcomes the customer in the ice cream shop.
 	 * @param name Name of the ice cream the customer ordered.
@@ -115,16 +126,16 @@ public class Eisdiele {
 	public void begruessen(String name) {
 		println("Guten Tag. Sie haben gerade ein " + name + " bestellt.");
 	}
-	
+
 	/**
 	 * Takes the money of the customer.
 	 * @param preis Price to pay for the ordered ice cream.
 	 */
 	public void kassieren(double preis) {
 		println("Das Eis kostet " + preis + "€.");
-		println("Vielen Dank fuer ihren Einkauf.");
+		println("Vielen Dank für ihren Einkauf.");
 	}
-	
+
 	/**
 	 * Says goodbye to the customer.
 	 */
@@ -134,17 +145,28 @@ public class Eisdiele {
 			println(" Und lassen Sie sich ihr Eis schmecken!");
 		}
 	}
-	
+
 	/**
 	 * Says sorry to the customer for not having the desired sort of ice cream.
 	 */
 	public void entschuldigen() {
 		println("Das gewünschte Eis ist leider nicht verfügbar. Versuchen Sie es doch mit einem anderen.");
+		print("Im Angebot habe wir: ");
+		for(int i = 0; i < eissorten.length; i++){
+			print(eissorten[i]);
+			if(i != eissorten.length - 1 && i != eissorten.length - 2){
+				print(", ");
+			}
+			else if(i == eissorten.length - 2){
+				print(" und ");
+			}
+		}
+		print("\n");
 	}
-	
+
 	public static void main(String[] args) {
 		Eisdiele diele = new Eisdiele();
-		
-		diele.bestellen("Spaghettieis");
+
+		diele.bestellen("Späg", 1);
 	}
 }
